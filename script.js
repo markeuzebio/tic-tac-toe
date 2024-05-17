850f110d01e1af64a87d426f66dbc5cb8148fcdb
Marcos Euzébio <markeuzebio@gmail.com>
2024-05-17 14:57:59 -0300

    Add insert token and print board to Gameboard func
    Some attributes inside "Gameboard" function had
    their name changed, now they are prefixed with
    _ symbol to indicate they behave as private
    attributes.
    
    Keyword "this" removed from inside Square function
    due malfunction of the program, it was a
    misconception of mine adding it.

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function Square()
 {
     let value = null;
 
-    const setValue = (value) => this.value = value;
-    const getValue = () => this.value;
+    const setValue = (token) => value = token;
+    const getValue = () => value;
 
     return {
         setValue: setValue,
@@ -13,15 +13,36 @@ function Square()
 
 function Gameboard()
 {
-    const ROWS = 3;
-    const COLUMNS = 3;
-    const board = [];
+    const _ROWS = 3;
+    const _COLUMNS = 3;
+    const _board = [];
 
-    for(let i = 0 ; i < ROWS ; i++)
+    for(let i = 0 ; i < _ROWS ; i++)
     {
-        board[i] = [];
+        _board[i] = [];
 
-        for(let j = 0 ; j < COLUMNS ; j++)
-            board[i].push(Square());
+        for(let j = 0 ; j < _COLUMNS ; j++)
+            _board[i].push(Square());
     }
+
+    const insertToken = function(row, column, token) {
+        if(row >= 0 && row < _ROWS && column >= 0 && column < _COLUMNS)
+        {
+            if(_board[row][column].getValue() == null)
+                _board[row][column].setValue(token);
+            else
+                console.log(`Position already taken by ${_board[row][column].getValue()}`);
+        }
+        else
+            console.log("NOT A VALID POSITION TO BE PUT");
+    };
+
+    const printBoard = function() {
+        console.log(_board.map((row) => row.map((cell) => cell.getValue())));
+    };
+
+    return {
+        insertToken: insertToken,
+        printBoard: printBoard
+    };
 }
