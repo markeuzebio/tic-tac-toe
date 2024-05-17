eaf2aa464a6ef56ab435cc447735069d0f30c2ba
Marcos Euzébio <markeuzebio@gmail.com>
2024-05-17 17:55:12 -0300

    Add a game controller to play the game via console
    Player object added.
    
    The game controller alternates between each
    player's turn time automatically. It also
    shows which player is supposed to play at a given
    moment.
    
    The method "insertToken" has now a return value.
    It's been added because the controller uses it
    to validate if a certain play from the user is
    valid or not.

--- script.js
+++ script.js
@@ -30,11 +30,17 @@ function Gameboard()
         {
             if(_board[row][column].getValue() == null)
                 _board[row][column].setValue(token);
-            else
+            else {
                 console.log(`Position already taken by ${_board[row][column].getValue()}`);
+                return false;
+            }
         }
-        else
+        else {
             console.log("NOT A VALID POSITION TO BE PUT");
+            return false;
+        }
+
+        return true;
     };
 
     const printBoard = function() {
@@ -44,5 +50,48 @@ function Gameboard()
     return {
         insertToken: insertToken,
         printBoard: printBoard
+    };
+}
+
+function Player(player_name, player_token)
+{
+    const name = player_name;
+    const token = player_token;
+
+    return {
+        getPlayerName: () => name,
+        getPlayerToken: () => token
+    }
+}
+
+function GameController(player_one_name = "Player 1", player_two_name = "Player 2")
+{
+    const _game_board = Gameboard();
+    const _player_one = Player(player_one_name, 'X');
+    const _player_two = Player(player_two_name, 'O');
+
+    let active_player = _player_one;
+
+    const printNewRound = function() {
+        console.log(`${active_player.getPlayerName()}'s (${active_player.getPlayerToken()}) turn.`);
+        _game_board.printBoard();
+    }
+
+    const switchPlayerTurn = () => active_player === _player_one ? active_player = _player_two : active_player = _player_one;
+
+    const playRound = function(row, column) {
+        console.log(`${active_player.getPlayerName()}'s turn. Try to put ${active_player.getPlayerToken()} on (${row}, ${column}).`);
+
+        // Checks if the play is valid
+        if(_game_board.insertToken(row, column, active_player.getPlayerToken()) === true)
+        {
+            switchPlayerTurn();
+            printNewRound();
+        }
+    }
+
+    return {
+        playRound: playRound,
+        getActivePlayer: () => active_player
     };
 }
